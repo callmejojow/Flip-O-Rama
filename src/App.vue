a91b471cd72eef29c58eaf680746ea877bcd5d8b
callmejojow <73418982+callmejojow@users.noreply.github.com>
2022-06-29 12:42:33 -0400

    refactor: create card component with value prop

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <h1>Flip-O-Rama</h1>
   <section class="game-board">
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
+    <AllCard 
+      v-for="(card, index) in cardList" 
+      :key="`card-${index}`" 
+      :value="card"
+    />
   </section>
 
 </template>
 
 <script>
 
+import AllCard from './components/AllCard'
+
+/* eslint-disable */
+
 export default {
   name: 'App',
   components: {
+    AllCard
+  },
+  setup(){
+    const cardList = []
 
+    for (let i = 0; i < 16; i++){
+      cardList.push(i)
+    }
+    return {
+      cardList
+    }
   }
 }
 </script>
---
+++ src/components/AllCard.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="card">
+		{{ value }}
+	</div>
+</template>
+
+
+<script>
+
+export default {
+	props:{
+		value: {
+			type: Number,
+			required: true
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
